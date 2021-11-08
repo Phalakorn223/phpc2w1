a5865cdad40982c7f806f004dd1b51f06d8b443e
Phalakorn223 <phalakorn061@gmail.com>
2021-11-08 11:38:17 +0700

    initial commit

---
+++ phpapp1.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP: Basic</title>
+    <style>
+        h1{
+            color:red;
+        }
+</head>
+
+<body>
+    <h1 style="color:blueviolet;">
+        <?php echo "PHP Syntax"; ?>
+    </h1>
+</body>
+
+</html>
